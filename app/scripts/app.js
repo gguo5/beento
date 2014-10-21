ee4acdcc246f6dd27107c27151dcc96fb46b7819
Grant <grant_guo@dataagility.com>
2014-10-21 17:04:52 +1100

    Signed-off-by: Grant <grant_guo@dataagility.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,6 +17,158 @@ var beenToApp = angular.module('beenToApp', [
 ]);
 
 
+beenToApp.filter('getCountryByName', function() {
+  return function(countries, name) {
+    var i=0, len=countries.length;
+    for (; i<len; i++) {
+      if (countries[i].name == name) {
+        return countries[i];
+      }
+    }
+    return null;
+  }
+});
+
+
+beenToApp.directive('map', function() {
+  return {
+        restrict: 'EAC', 
+        link: function(scope, element, attrs) {
+          var chart = null;
+  
+           scope.$watch("datamap" , function(n,o){ 
+             if(!chart){
+                $(element).width('auto')
+                $(element).height(400)
+                chart = $(element).vectorMap({
+                   map: 'world_mill_en_with_antarctica',
+                   normalizeFunction: 'polynomial',
+                   regionStyle: {
+                      initial: {
+                          fill: 'white',
+                          "fill-opacity": 1,
+                          stroke: 'none',
+                          "stroke-width": 0,
+                          "stroke-opacity": 1
+                      },
+                      hover: {
+                          fill: '#CCECE0',
+                          "fill-opacity": 0.8
+                      }
+                    },
+                    markerStyle: {
+                      initial: {
+                          fill: '#FA4239',
+                          stroke: '#FFFFFF'
+                      },
+                      hover: {
+                          stroke: '#1A8EBF',
+                          "stroke-width": 2
+                      }
+                  },
+                  backgroundColor: '#96CBD7',
+                  markers: [
+                  //{latLng: [41.90, 12.45], name: 'Vatican City'},
+                  //{latLng: [43.93, 12.46], name: 'San Marino'},
+                  //{latLng: [-4.61, 55.45], name: 'Seychelles'},
+                  {latLng: [7.35, 134.46], name: 'Palau'}
+                  ],
+                series: {
+                  regions: [{
+                    //values: scope.datamap,  
+                     //scale: ['#0071A4'],
+                       scale: {
+                        "150": "#2761ad",
+                        "009": "#0071A4",
+                        "142": "#8019B3",
+                        "002": "#E0B84E",
+                        "019": "#56EA4C",
+                        "others": "#66EADB"
+                    },
+                    attribute:'fill'
+                  }]
+                }
+                
+                })
+             }else{
+                chart.vectorMap('get', 'mapObject').series.regions[0].setValues(scope.datamap);
+                console.log(chart.vectorMap('get', 'mapObject'));
+             }
+          });              
+        }
+    }; 
+
+
+
+    // return {
+    //     restrict: 'A',
+    //     link: function(scope, element, attrs) {
+    //       var chart = null;
+    //       scope.$watch("datamap" , function(n,o){
+    //       console.log('watch1 '+chart);
+    //      if(!chart){
+    //     $(element).width('auto')
+    //     $(element).height(400)
+    //        $(element).vectorMap({
+    //             map: 'world_mill_en_with_antarctica',
+    //               normalizeFunction: 'polynomial',
+    //               regionStyle: {
+    //                   initial: {
+    //                       fill: 'white',
+    //                       "fill-opacity": 1,
+    //                       stroke: 'none',
+    //                       "stroke-width": 0,
+    //                       "stroke-opacity": 1
+    //                   },
+    //                   hover: {
+    //                       fill: '#CCECE0',
+    //                       "fill-opacity": 0.8
+    //                   }
+    //               },
+    //               markerStyle: {
+    //                   initial: {
+    //                       fill: '#FA4239',
+    //                       stroke: '#FFFFFF'
+    //                   },
+    //                   hover: {
+    //                       stroke: '#1A8EBF',
+    //                       "stroke-width": 2
+    //                   }
+    //               },
+    //               backgroundColor: '#96CBD7',
+    //               markers: [
+    //               //{latLng: [41.90, 12.45], name: 'Vatican City'},
+    //               //{latLng: [43.93, 12.46], name: 'San Marino'},
+    //               //{latLng: [-4.61, 55.45], name: 'Seychelles'},
+    //               {latLng: [7.35, 134.46], name: 'Palau'}
+    //               ],
+    //                series: {
+    //                 regions: [{
+    //                   //values: {"AU": "oceania"},
+    //                  scale: ['#0071A4'],
+    //                 //    scale: {
+    //                 //     "150": "#2761ad",
+    //                 //     "009": "#0071A4",
+    //                 //     "142": "#8019B3",
+    //                 //     "002": "#E0B84E",
+    //                 //     "019": "#56EA4C",
+    //                 //     undefined: "#c7b789"
+    //                 // },
+    //                   attribute: 'fill'
+    //                 }]
+    //               }
+    //           })
+    //       }
+    //       else{
+    //         console.log('watch else '+ chart);
+    //             chart.vectorMap('get', 'mapObject').series.regions[0].setValues(scope.datamap);
+    //             console.log(chart.vectorMap('get', 'mapObject'))};        
+    //   });              
+    //     }
+    // };
+});
+
+
 beenToApp.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,14 +8,28 @@
  * Controller of the beenToApp
  */
 angular.module('beenToApp')
-  .controller('MainCtrl', function ($scope,$http) {
+  .controller('MainCtrl', function ($scope,$http,$filter) {
     $http.get('data/allcountries.json').success(function(data) {
     $scope.countries = data;
+    $scope.typedCountry = undefined;
+    $scope.datamap = {};
     $scope.regions = [{"name":"Africa","code":"002"},
     				  {"name":"Americas","code":"019"},
     				  {"name":"Asia","code":"142"},
     				  {"name":"Europe","code":"150"},
     				  {"name":"Oceania","code":"009"},
-    				  {"name":"Others"}]
+    				  {"name":"Others"}];
+    $scope.getCountryByName = function(c){
+      var found = $filter('getCountryByName')($scope.countries, c);
+      $scope.triggerAlert(found);
+    }; 
+
+    $scope.triggerAlert = function(c){
+      var r={};
+      typeof c['region-code'] === "undefined" ? r[c['alpha-2']]="others" : r[c['alpha-2']]=c['region-code'];  
+      $scope.datamap=r;
+      console.log($scope.datamap);
+    };
+    
   });
   });
